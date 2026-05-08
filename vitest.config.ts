97adf4c5b31babba19115a0c50f5df8f40468ffe
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2026-05-08 08:06:38 +0700

    chore: revert vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,20 +2,12 @@ import { resolve } from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "./src"),
-        },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
     },
-    test: {
-        environment: "node",
-        env: {
-            NEXT_PHASE: "phase-production-build",
-            DATABASE_URL: "postgres://test",
-            BETTER_AUTH_SECRET: "test-secret-32-chars-long!!",
-            ENCRYPTION_KEY:
-                "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
-            APP_URL: "http://localhost:3000",
-        },
-    },
+  },
+  test: {
+    environment: "node",
+  },
 });
